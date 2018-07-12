309c848b29c39326d215f3ecfb09e59e12802b0e
phani <phani@lancia.cs.uga.edu>
2018-07-12 09:03:17 -0400

    docker setup script

---
+++ setup_docker.sh
@@ -0,0 +1,24 @@
+# From: https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-docker-ce-1
+# Set up Docker APT Repository
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# Install Docker CE
+sudo apt-get update
+sudo apt-get install docker-ce
+
+# Create a docker group and add this user there 
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+echo "Log out and login and try this command to make sure the installation went smoothly:"
+echo "docker run hello-world"
